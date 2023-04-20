20af958c6e4df826bb2f32658504dc743a74f436
eihap <eihapinfo@gmail.com>
2023-04-20 17:01:14 +0300

    install Microsoft.Extensions.DependencyInjection packdge and create program code

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace CalculatorApp
+﻿using Bl;
+using CalculatorApp.Utlities;
+using Domains;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CalculatorApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args, NormalCalModel model, SincCalModel sincmodel)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a new instance of the ServiceCollection class
+            var services = new ServiceCollection();
+
+            // Register the required services and their implementations
+            services.AddScoped<IBusinessLayer<NormalCalModel>, ClsNormalCal>();
+            services.AddScoped<IBusinessLayer<SincCalModel>, ClsSincCalBl>();
+            // Build the service provider
+            var serviceProvider = services.BuildServiceProvider();
+
+            string sVarSwitch = string.Empty;
+            while (sVarSwitch != "e")
+            {
+                UiHelper uiHelper = new UiHelper
+                (
+                    serviceProvider.GetService<IBusinessLayer<NormalCalModel>>(),
+                    serviceProvider.GetService<IBusinessLayer<SincCalModel>>()
+                 );
+
+                uiHelper.MainOptions();
+                sVarSwitch = Console.ReadLine();
+                switch (sVarSwitch)
+                {
+                    case "e":
+                        sVarSwitch = "e";
+                        break;
+                    case "1":
+                        Console.Clear();
+                        uiHelper.NormalCalOptions(model);
+                        break;
+                    case "2":
+                        Console.Clear();
+                        uiHelper.SinCalOptions(sincmodel);
+                        break;
+                    case "3":
+                        break;
+                }
+
+            }
         }
     }
 }
--- CalculatorApp/Utlities/UiHelper.cs
+++ CalculatorApp/Utlities/UiHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bl;
+using Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,117 @@ using System.Threading.Tasks;
 
 namespace CalculatorApp.Utlities
 {
-    internal class UiHelper
+    public class UiHelper
     {
+        IBusinessLayer<NormalCalModel> oClsNormalCal;
+        IBusinessLayer<SincCalModel> oClsSincCal;
+        public UiHelper(IBusinessLayer<NormalCalModel> normalCalModel, IBusinessLayer<SincCalModel> sincCalModel)
+        {
+            oClsNormalCal = normalCalModel;
+            oClsSincCal = sincCalModel;
+        }
+        public void MainOptions()
+        {
+
+
+
+            Console.WriteLine("*** wlc to cal app :\n");
+            Console.WriteLine("press e to exit  :\n");
+            Console.WriteLine("press 1 to normal calculator :\n");
+            Console.WriteLine("press 2 to scientific calculator :\n");
+            Console.WriteLine("press 3 to proff calculator  :\n");
+        }
+
+        #region NormalCalOptions
+
+        public void NormalCalOptions(NormalCalModel model)
+        {
+
+            string sVarSwitch = string.Empty;
+            while (sVarSwitch != "b")
+            {
+                Console.WriteLine("press b to back  :\n");
+                Console.WriteLine("press 1 to add number  :\n");
+                Console.WriteLine("press 2 to sub number  :\n");
+                Console.WriteLine("press 3 to mul number  :\n");
+                Console.WriteLine("press 4 to div number  :\n");
+
+                sVarSwitch = Console.ReadLine();
+
+
+                switch (sVarSwitch)
+                {
+                    case "b":
+                        Console.Clear();
+                        sVarSwitch = "b";
+                        break;
+                    case "1":
+                        Console.Clear();
+
+
+                        oClsNormalCal.Add(model);
+                        break;
+                    case "2":
+                        Console.Clear();
+
+                        oClsNormalCal.Sub(model);
+                        break;
+                    case "3":
+                        Console.Clear();
+
+                        oClsNormalCal.Mul(model);
+                        break;
+                    case "4":
+                        Console.Clear();
+
+                        oClsNormalCal.Div(model);
+                        break;
+                }
+
+            }
+        }
+        #endregion
+
+
+        #region SinCalOptions
+        public void SinCalOptions(SincCalModel sincmodel)
+        {
+            string sVarSwitch = string.Empty;
+            while (sVarSwitch != "b")
+            {
+                Console.WriteLine("press b to back  :\n");
+                Console.WriteLine("press 1 to sin (number)  :\n");
+                Console.WriteLine("press 2 to cos (number)  :\n");
+                Console.WriteLine("press 3 to tan (number)  :\n");
+
+                sVarSwitch = Console.ReadLine();
+
+                switch (sVarSwitch)
+                {
+                    case "b":
+                        Console.Clear();
+                        sVarSwitch = "b";
+                        break;
+                    case "1":
+                        Console.Clear();
+
+                        oClsSincCal.Sin(sincmodel);
+                        break;
+                    case "2":
+                        Console.Clear();
+
+                        oClsSincCal.Cos(sincmodel);
+                        break;
+                    case "3":
+                        Console.Clear();
+
+                        oClsSincCal.tan(sincmodel);
+                        break;
+
+                }
+
+            }
+        }
+        #endregion
     }
 }
